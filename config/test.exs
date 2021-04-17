946373d2021ebab62e559e1699e846244d611b01
himangshuj <hjhazarika@gmail.com>
2021-04-17 14:30:30 -0700

    test environment variable fixes for github actions
    Signed-off-by: himangshuj <hjhazarika@gmail.com>

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :phoenix_boilerplate, PhoenixBoilerplate.Repo,
   username: "postgres",
   password: "postgres",
   database: "phoenix_boilerplate_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "#{System.get_env("DB_HOST") || "localhost"}",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
