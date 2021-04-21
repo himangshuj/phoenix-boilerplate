6eec559a267dc5ff7687104f7bd1d34e4405d850
himangshuj <hjhazarika@gmail.com>
2021-04-21 14:21:05 -0700

    releases configuration

--- config/releases.exs
+++ config/releases.exs
@@ -1,12 +1,4 @@
 import Config
-config :phoenix_boilerplate, PhoenixBoilerplateWeb.Endpoint,
-       url: [host: nil, port: 80],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]],
-       cache_static_manifest: "priv/static/cache_manifest.json"
-
-# Do not print debug messages in production
-config :logger, level: :info
-
 database_url =
   System.get_env("DATABASE_URL") ||
     raise """
@@ -17,4 +9,18 @@ database_url =
 config :phoenix_boilerplate, PhoenixBoilerplate.Repo,
        # ssl: true,
        url: database_url,
-       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :phoenix_boilerplate, PhoenixBoilerplateWeb.Endpoint,
+       http: [
+         port: String.to_integer(System.get_env("PORT") || "4000"),
+         transport_options: [socket_opts: [:inet6]]
+       ],
+       secret_key_base: secret_key_base
